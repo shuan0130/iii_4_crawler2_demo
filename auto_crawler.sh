9e8b17aae1b14a0c49132c012b543fd1eafc9f65
shuan0130 <38446201+shuan0130@users.noreply.github.com>
2018-08-20 16:39:09 +0800

    Update auto_crawler.sh

--- auto_crawler.sh
+++ auto_crawler.sh
@@ -5,5 +5,4 @@
 #2018-07-25	Howard
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-python3 ~/Desktop/crawler2_demo.py
-
+*/10 * * * * python3 ~/Desktop/crawler2_demo.py #At every 10th minute
